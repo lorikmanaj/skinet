a509c9d21a729b2a77d4c2b5548401eb8f56d50d
Lik-ABC <lorik.manaj@sqa-consulting.com>
2021-11-19 18:17:32 +0100

    Added .gitignore, nuget package for EFCore (sqlite), and base classes

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly ILogger<ProductsController> _logger;
+
+        public ProductsController(ILogger<ProductsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string GetProducts() {
+            return "Get Prodss";
+        }
+
+        [HttpGet("{id}")]
+        public string GetProduct(int id) {
+            return "single prod";
+        }
+
+        // [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        // public IActionResult Error()
+        // {
+        //     return View("Error!");
+        // }
+    }
+}
