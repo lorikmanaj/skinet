1d4359fd3949c4b2cc67d2d0304fffbfe8eacaaf
Lik-ABC <lorik@DESKTOP-71QNDUD>
2021-11-25 11:45:20 +0100

    Injected Context and general config

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using API.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
@@ -12,10 +13,12 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class ProductsController : ControllerBase
     {
+        private readonly StoreContext _context;
         private readonly ILogger<ProductsController> _logger;
 
-        public ProductsController(ILogger<ProductsController> logger)
+        public ProductsController(StoreContext context, ILogger<ProductsController> logger)
         {
+            _context = context;
             _logger = logger;
         }
 
